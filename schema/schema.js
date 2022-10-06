52a94d4ff18d6186390647d6d86affe785da05a2
youngbo choi <youngbosyd@hotmail.com>
2022-10-06 11:13:04 +0900

    fixed indentation and added resolve method in user

--- schema/schema.js
+++ schema/schema.js
@@ -1,31 +1,47 @@
 const graphql = require('graphql');
 const {GraphQLObjectType, GraphQLString, GraphQLInt,
-        GraphQLSchema } = graphql;
+  GraphQLSchema } = graphql;
 const axios = require('axios');
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
 const UserType = new GraphQLObjectType({
-    name: 'User',
-    fields: {
+  name: 'User',
+  fields: {
       id: { type: GraphQLString },
       firstName: { type: GraphQLString },
-      age: { type: GraphQLInt }
-    }
+      age: { type: GraphQLInt },
+      company: { 
+      type :CompanyType,
+      resolve(parentValue, args){        
+              return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                  .then(res=>res.data)
+          }
+      },
+  }
 });
 
 const RootQuery = new GraphQLObjectType({
-    name:'RootQueryType',
-    fields:{
-        user:{
-            type:UserType,
-            args:{id : { type :GraphQLString}},
-            resolve(parentValue, args){
-                return axios.get(`http://localhost:3000/users/${args.id}`)
-                    .then(resp =>resp.data);
-            }
-        }
-    }
+  name:'RootQueryType',
+  fields:{
+      user:{
+          type:UserType,
+          args:{id : { type :GraphQLString}},
+          resolve(parentValue, args){
+              return axios.get(`http://localhost:3000/users/${args.id}`)
+                  .then(resp =>resp.data);
+          }
+      }
+  }
 });
 
 module.exports = new GraphQLSchema({
-    query:RootQuery
+  query:RootQuery
 });
