f95fd6194b99ade23ca0352b78583638bac29fb4
youngbo choi <youngbosyd@hotmail.com>
2022-10-06 13:28:50 +0900

    study bidirectional relations

--- schema/schema.js
+++ schema/schema.js
@@ -1,31 +1,38 @@
 const graphql = require('graphql');
 const {GraphQLObjectType, GraphQLString, GraphQLInt,
-  GraphQLSchema } = graphql;
+  GraphQLSchema,GraphQLList } = graphql;
 const axios = require('axios');
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields:()=>({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    description: { type: GraphQLString }
-  }
+    description: { type: GraphQLString },
+    users:{
+      type:new GraphQLList(UserType),
+      resolve(parentValue, args){
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then(res=>res.data);
+      }
+    }
+  })
 });
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: ()=>({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
     company: { 
-    type :CompanyType,
-    resolve(parentValue, args){        
-      return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
-          .then(res=>res.data)
-      }
+      type :CompanyType,
+      resolve(parentValue, args){        
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+            .then(res=>res.data)
+        }
     },
-  }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
