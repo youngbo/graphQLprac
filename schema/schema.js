739c187a9b6875613098192fb95f8701ff442475
youngbo choi <youngbosyd@hotmail.com>
2022-10-06 11:34:36 +0900

    multiple rootQuery Entry Point

--- schema/schema.js
+++ schema/schema.js
@@ -15,30 +15,38 @@ const CompanyType = new GraphQLObjectType({
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
-      id: { type: GraphQLString },
-      firstName: { type: GraphQLString },
-      age: { type: GraphQLInt },
-      company: { 
-      type :CompanyType,
-      resolve(parentValue, args){        
-              return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
-                  .then(res=>res.data)
-          }
-      },
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    company: { 
+    type :CompanyType,
+    resolve(parentValue, args){        
+      return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(res=>res.data)
+      }
+    },
   }
 });
 
 const RootQuery = new GraphQLObjectType({
   name:'RootQueryType',
   fields:{
-      user:{
-          type:UserType,
-          args:{id : { type :GraphQLString}},
-          resolve(parentValue, args){
-              return axios.get(`http://localhost:3000/users/${args.id}`)
-                  .then(resp =>resp.data);
-          }
+    user:{
+      type:UserType,
+      args:{id : { type :GraphQLString}},
+      resolve(parentValue, args){
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+            .then(resp =>resp.data);
+        }
+    },
+    company:{
+      type:CompanyType,
+      args:{id : {type: GraphQLString}},
+      resolve(parentValue, args){
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
+          .then(resp=>resp.data);
       }
+    }
   }
 });
 
